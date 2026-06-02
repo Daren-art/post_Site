97955f1adf61417e3515944995fbf59a98dc24e6
Daren-art <fna33416@gmail.com>
2026-06-02 15:32:46 +0500

    Fix nested repository

---
+++ blog/post/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    image = models.ImageField(upload_to='/posts', blank=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category , on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=50)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
